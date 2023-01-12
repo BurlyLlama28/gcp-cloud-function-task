0b05002c5ec6dfecb8ef282b52d33a8db3c1c289
bkost <bkost@softserveinc.com>
2023-01-12 13:38:04 +0200

    Update main.tf #11

--- cf-task/terraform/main.tf
+++ cf-task/terraform/main.tf
@@ -114,8 +114,8 @@ resource "google_cloudfunctions_function" "task-cf-function" {
 }
 
 resource "google_cloudfunctions_function_iam_member" "invoker" {
-  project        = google_cloudfunctions_function.task-cf-function.project
-  region         = google_cloudfunctions_function.task-cf-function.region
+  project        = var.project_id
+  region         = var.region
   cloud_function = google_cloudfunctions_function.task-cf-function.name
 
   role   = "roles/cloudfunctions.invoker"
